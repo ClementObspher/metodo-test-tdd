b754f9ee1300be89868e118caa7e43f70bf1e762
ClementObspher <144014423+ClementObspher@users.noreply.github.com>
2025-02-27 11:34:58 +0100

    Update carpaccio.test.ts

--- test/carpaccio.test.ts
+++ test/carpaccio.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from "bun:test"
 import { calculateTtcPrice, applyDiscount } from "../src/carpaccio"
 
 test("Calcul du prix brut", () => {
-	expect(calculateTtcPrice(100, "CA")).toBeCloseTo(1028.75, 2)
-	expect(calculateTtcPrice(200, "TX")).toBeCloseTo(1062.5, 2)
+	expect(calculateTtcPrice(100, "CA")).toBeCloseTo(108.25, 2)
+	expect(calculateTtcPrice(200, "TX")).toBeCloseTo(212.5, 2)
 })
 
 test("Application des réductions", () => {
@@ -13,8 +13,8 @@ test("Application des réductions", () => {
 })
 
 test("Gestion des taxes", () => {
-	expect(calculateTtcPrice(100, "UT")).toBeCloseTo(1976, 2)
-	expect(calculateTtcPrice(50, "CA")).toBeCloseTo(4770, 2)
+	expect(calculateTtcPrice(100, "UT")).toBeCloseTo(197.6, 2)
+	expect(calculateTtcPrice(50, "CA")).toBeCloseTo(477.0, 2)
 })
 
 test("État non pris en charge", () => {
