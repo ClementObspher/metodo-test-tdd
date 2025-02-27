e46fb52cf48cdfc50784ec98508996cee4df9fcb
ClementObspher <144014423+ClementObspher@users.noreply.github.com>
2025-02-27 11:23:56 +0100

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,30 +1,30 @@
-import { expect, test } from "bun:test"
-import { intToRoman } from "../src/intToRoman"
+// import { expect, test } from "bun:test"
+// import { intToRoman } from "../src/intToRoman"
 
-test("convert 0 to Roman numeral", () => {
-	expect(intToRoman(0)).toBe("")
-})
+// test("convert 0 to Roman numeral", () => {
+// 	expect(intToRoman(0)).toBe("")
+// })
 
-test("convert 1 to Roman numeral", () => {
-	expect(intToRoman(1)).toBe("I")
-})
+// test("convert 1 to Roman numeral", () => {
+// 	expect(intToRoman(1)).toBe("I")
+// })
 
-test("convert 1994 to Roman numeral", () => {
-	expect(intToRoman(1994)).toBe("MCMXCIV")
-})
+// test("convert 1994 to Roman numeral", () => {
+// 	expect(intToRoman(1994)).toBe("MCMXCIV")
+// })
 
-test("convert 58 to Roman numeral", () => {
-	expect(intToRoman(58)).toBe("LVIII")
-})
+// test("convert 58 to Roman numeral", () => {
+// 	expect(intToRoman(58)).toBe("LVIII")
+// })
 
-test("convert 9 to Roman numeral", () => {
-	expect(intToRoman(9)).toBe("IX")
-})
+// test("convert 9 to Roman numeral", () => {
+// 	expect(intToRoman(9)).toBe("IX")
+// })
 
-test("convert negative number to Roman numeral", () => {
-	expect(() => intToRoman(-1)).toThrow("Number must be positive")
-})
+// test("convert negative number to Roman numeral", () => {
+// 	expect(() => intToRoman(-1)).toThrow("Number must be positive")
+// })
 
-test("convert number >= 3999 to Roman numeral", () => {
-	expect(() => intToRoman(3999)).toThrow("Number must be less than 3999")
-})
+// test("convert number >= 3999 to Roman numeral", () => {
+// 	expect(() => intToRoman(3999)).toThrow("Number must be less than 3999")
+// })
